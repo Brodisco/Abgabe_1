aac57f141e2fb18a8e9bf5ed84346bfd8b5bd62d
Brodisco <115808535+Brodisco@users.noreply.github.com>
2022-10-14 18:15:02 +0200

    Update main.c

--- Abgabe1/src/main.c
+++ Abgabe1/src/main.c
@@ -11,6 +11,16 @@
 int main(void)
 {
 	printf("Abgabe 1: 28.10.22 \n");
-
+	
+	//AUFGABE 1
+	/*
+	printf("   (%c`-''-/%c).___..--''%c`-._  	\n",34,34,34);
+	printf("    `6_ 6 ) `-. (     ).`-.__.`)     	\n");
+	printf("    (_Y_.)' ._ ) `._ `. ``-..-'  	\n");
+	printf("  _..`--'_..-_/ /--'_.' ,'			\n");
+	printf(" (il),-'' (li),' ((!.-' 			\n");
+	*/
+	
+	
 	return EXIT_SUCCESS;
 }
