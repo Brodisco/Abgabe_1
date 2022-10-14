35c11b02b56c7eb2389f839e9219971ecb390737
student <student@ubuntu>
2022-10-14 08:48:37 -0700

    Neues Projekt angelegt

---
+++ Abgabe1/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 14.10.2022
+ *      Author: student
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Abgabe 1: 28.10.22 \n");
+
+	return EXIT_SUCCESS;
+}
