9e6aad83b4b16bff2037b1ccf8d8333a0c819bef
Brodisco <115808535+Brodisco@users.noreply.github.com>
2022-10-14 11:29:37 -0700

    Maximilian: Das Aufgabe ein und zwei von S gelösch

--- Abgabe1/src/main.c
+++ Abgabe1/src/main.c
@@ -12,28 +12,5 @@ int main(void)
 {
 	printf("Abgabe 1: 28.10.22 \n");
 	
-	//AUFGABE 1
-	/*
-	printf("   (%c`-''-/%c).___..--''%c`-._  	\n",34,34,34);
-	printf("    `6_ 6 ) `-. (     ).`-.__.`)     	\n");
-	printf("    (_Y_.)' ._ ) `._ `. ``-..-'  	\n");
-	printf("  _..`--'_..-_/ /--'_.' ,'			\n");
-	printf(" (il),-'' (li),' ((!.-' 			\n");
-	*/
-	
-	
-	//AUFGABE 2
-	/*
-	int Zahl = 1117; // <- Zahl für Quersumme
-
-	int TBereich = Zahl / 1000 ;
-	int HBereich = Zahl / 100 - TBereich * 10;
-	int ZBereich = Zahl / 10 - TBereich * 100 - HBereich * 10;
-	int EBereich = Zahl - TBereich * 1000 - HBereich * 100 -ZBereich * 10;
-
-	int Quersumme = TBereich + HBereich + ZBereich + EBereich;
-	printf("%d",Quersumme);
-	*/
-	
 	return EXIT_SUCCESS;
 }
