e68631cb9f310311b4ba15c535f197a283cd571f
Brodisco <115808535+Brodisco@users.noreply.github.com>
2022-10-14 18:42:55 +0200

    Update main.c

--- Abgabe1/src/main.c
+++ Abgabe1/src/main.c
@@ -22,5 +22,18 @@ int main(void)
 	*/
 	
 	
+	//AUFGABE 2
+	/*
+	int Zahl = 1117; // <- Zahl für Quersumme
+
+	int TBereich = Zahl / 1000 ;
+	int HBereich = Zahl / 100 - TBereich * 10;
+	int ZBereich = Zahl / 10 - TBereich * 100 - HBereich * 10;
+	int EBereich = Zahl - TBereich * 1000 - HBereich * 100 -ZBereich * 10;
+
+	int Quersumme = TBereich + HBereich + ZBereich + EBereich;
+	printf("%d",Quersumme);
+	*/
+	
 	return EXIT_SUCCESS;
 }
